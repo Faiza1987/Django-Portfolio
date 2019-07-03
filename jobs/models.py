7e35a92106ded297c2ce1d39540d55fcb4cb23d4
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-07-03 16:10:56 -0700

    Created Homepage using Bootstrap

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# models.Model allows us to create a Job class along with some background django code we need
+
+class Job(models.Model):
+    # upload_to="" -> inside the media folder, we can create additional folders to upload our images to.
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=250)
